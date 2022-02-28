08f842b1efffc17f8f32ed36ee0bca8c20c24930
guzdziac55 <guzdziac55@gmail.com>
2022-02-28 22:33:54 +0100

    arrayRefs idea

--- src/components/AutoCompleteDropdown.tsx
+++ src/components/AutoCompleteDropdown.tsx
@@ -1,4 +1,5 @@
 import { Suggestions } from './types'
+import { useEffect, useRef } from 'react'
 import classes from './AutoCompleteDropdown.module.css'
 
 interface AutoCompleteDropdownProps {
@@ -12,13 +13,31 @@ const AutoCompleteDropdown: React.FC<AutoCompleteDropdownProps> = ({
     active,
     onChange,
 }) => {
+    const itemsRef = useRef([]) // array of  refs
+    //  itemsRef.current[n]
+
+    // useEffect(() => {
+    //     itemsRef.current = itemsRef.current.slice(0, filtered.length)
+    // }, [filtered])
+
+    // if index === active    => ref.current.focus()   <=////
+
+    useEffect(() => {
+        console.log(itemsRef.current[active])
+    }, [active])
+
     return (
         <ul>
             {filtered.map((item: string, index: number) => {
                 const activeStyle = index === active ? classes.active : ''
 
+                // we need focus here <=====  ////  from 0 to arraySuggestions.length
+                //  if focus === index = >      => focus li === true !
+                //  if li === true     =>      ref => current .focus()
+
                 return (
                     <li
+                        ref={itemsRef.current[index]}
                         tabIndex={0}
                         onClick={() => {
                             onChange(item)
